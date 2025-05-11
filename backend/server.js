0807a021216267a7d17c898e24f28ede7b9eeb39
MuhammadTayyebHamid <159242117+MuhammadTayyebHamid@users.noreply.github.com>
2025-05-11 13:16:56 +0500

    Latest

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal server error' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+  });
